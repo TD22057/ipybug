32da70516748c0486d6913ec659d07fcbed9b041
drain <ted.drain@jpl.nasa.gov>
2020-03-03 19:42:15 -0800

    PIP issue #7817

---
+++ ipy_script.py
@@ -0,0 +1,4 @@
+#!/bin/env ipython
+
+print("Testing 1 2 3")
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import glob
+import setuptools
+
+setuptools.setup(
+    name = 'ipy_test',
+    version = '0.1.0',
+    description = 'short description',
+    url = 'https://github.com/TD22057/ipy_test',
+    scripts = ["ipy_script.py"],
+    )
+
